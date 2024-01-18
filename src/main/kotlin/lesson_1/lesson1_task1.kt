7ccbcb607b36b592e4120f3ae3e02dd47d7ae2b2
gotovkk <animationt111@gmail.com>
2024-01-18 12:36:30 +0300

    KS-1-1 The variable of the number of orders and the text with the benefit are made

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,7 +1,7 @@
 package lesson_1
 
 fun main() {
-    val ordersAmount: Int = 33  // Количество заказов
+    val ordersAmount: Int = 75  // Количество заказов
 
     val thanksGiving: String = "Благодарим вас за доверие к нашему бренду! Уверены, что вы останетесь довольны нашей продукцией :)" // Текст с благодарностью
 
