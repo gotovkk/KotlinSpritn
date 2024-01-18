29c12d56e095d60fc5aeb5035f7066de63b6dac1
gotovkk <animationt111@gmail.com>
2024-01-18 13:33:03 +0300

    KS-1-3 Solution of task about space

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,2 +1,16 @@
 package lesson_1
 
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Год полёта: $year")
+    println("Время взлёта(часы): $hour")
+    println("Время взлёта(минуты): $minute")
+    // Изменение значений для печати времени посадки
+    hour = 10
+    minute = 55
+
+    println("Время посадки - ${String.format("%02d", hour)}:${String.format("%02d", minute)}")
+
+}
