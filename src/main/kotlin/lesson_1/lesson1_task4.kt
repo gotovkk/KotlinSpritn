2571b86529013eb64122a8683e2a0588f683856d
gotovkk <animationt111@gmail.com>
2024-01-18 14:16:45 +0300

    KS-1-4 data types

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,10 @@
+package lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: UByte = 27u
+    val partOfDay: Float = 0.0875f
+    val seconds: UShort = 6480u
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+}
