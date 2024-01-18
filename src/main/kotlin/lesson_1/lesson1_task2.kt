d1b818ae8b02794c76c43ef42a65a5ff3b601df5
gotovkk <animationt111@gmail.com>
2024-01-18 12:53:15 +0300

    KS-1-2 The number of employees has been added

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -2,7 +2,7 @@ package lesson_1
 
 fun main() {
     val ordersAmount: Int = 75  // Количество заказов
-    val thanksGiving: String = "Благодарим вас за доверие к нашему бренду! Уверены, что вы останетесь довольны нашей продукцией :)" // Текст с благодарностью
+    val thanksGiving: String = "Благодарим вас за доверие к нашему бренду! Уверены, что вы останетесь довольны нашей продукцией:)" // Текст с благодарностью
     var numOfEmployees: Int = 2000
 
     println(ordersAmount)
