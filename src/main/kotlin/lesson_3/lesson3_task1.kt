f252a7cca2f1513d54063779ace5aff398ee8524
gotovkk <animationt111@gmail.com>
2024-01-25 02:10:04 +0300

    KS-3-1 app greetings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main() {
+    val name: String = "Евгений"
+    var greetings: String = "Добрый день, $name!"
+    println(greetings)
+    greetings = "Добрый вечер, $name!"
+    println(greetings)
+}
