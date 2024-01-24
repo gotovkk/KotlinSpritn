ed814590c96419d02a8cc109c45d5c34223cfc60
gotovkk <animationt111@gmail.com>
2024-01-25 02:11:02 +0300

    KS-3-2 person data

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    val firstName: String = "Татьяна"
+    val middleName: String = "Сергеевна"
+    var lastName: String = "Андреева"
+    var age: Int = 20
+    println("[$lastName $firstName $middleName, $age]")
+    lastName = "Сидорова"
+    age = 22
+    println("[$lastName $firstName $middleName, $age]")
+
+}
