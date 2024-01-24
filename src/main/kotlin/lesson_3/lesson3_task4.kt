4b617c9cdf704627f7bbfffcba1df7c38740b157
gotovkk <animationt111@gmail.com>
2024-01-25 02:13:21 +0300

    KS-3-4 chess positions

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,10 @@
+package lesson_3
+
+fun main() {
+    var startPos: String = "E2"
+    var endPos: String = "E4"
+    println("[$startPos;$endPos]")
+    startPos = "D2"
+    endPos = "D3"
+    println("[$startPos;$endPos]")
+}
