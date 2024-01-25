7ea5c1aecfc83466c0e90ccfbd5cb660c5790399
gotovkk <animationt111@gmail.com>
2024-01-25 15:10:56 +0300

    KS-3-4 chess move count added

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -3,8 +3,10 @@ package lesson_3
 fun main() {
     var startPos: String = "E2"
     var endPos: String = "E4"
-    println("[$startPos;$endPos]")
+    var moveCount: Int = 0
+    println("[$startPos - $endPos; $moveCount]")
     startPos = "D2"
     endPos = "D3"
-    println("[$startPos;$endPos]")
+    moveCount += 1
+    println("[$startPos - $endPos: $moveCount]")
 }
