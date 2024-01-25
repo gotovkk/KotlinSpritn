215a59846380e0b6923f8861a567accf1cab1ef7
gotovkk <animationt111@gmail.com>
2024-01-25 19:48:32 +0300

    KS-4-1 tables become const

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,17 +1,18 @@
 package lesson_4
 
 fun main() {
-    val tables = 13
     var bookedToday: Int = 10
     var bookedTomorrow: Int = 13
     var isBookedToday: Boolean = false
     var isBookedTomorrow: Boolean = false
 
-    isBookedToday = tables != bookedToday
+    isBookedToday = TABLES != bookedToday
 
-    isBookedTomorrow = tables != bookedTomorrow
+    isBookedTomorrow = TABLES != bookedTomorrow
 
     println("[Доступность столиков на сегодня: $isBookedToday]")
     println("[Доступность столиков на сегодня: $isBookedTomorrow]")
 
 }
+
+const val TABLES = 13
